879712a4fb9769dd7dae969a5ac56c98ba8c884c
D0d0 <jozef.d13@gmail.com>
2014-10-19 12:21:19 +0200

    app first look

--- magazine/app/views/onas.blade.php
+++ magazine/app/views/onas.blade.php
@@ -1,43 +1,40 @@
-@extends('master')
+@extends('two_one_layout')
 
-@section('content')
+@section('left')
 <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-6">
         <div class="row">
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="thumbnail clearfix">
-                            <img src="{{URL::asset('img/apache_pb.png')}}" alt="..." class="img-thumbnail pull-left">
-                            <h3>Meno priezvisko</h3>
-                            <p><span class="glyphicon glyphicon-envelope"></span> jozef.d13@gmail.com</p>
-                            <p><span class="glyphicon glyphicon-map-marker"></span> FMFI UK, Bratislava</p>    
-                            <p class="text-justify"><span class="glyphicon glyphicon-search"></span> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean et est a dui semper facilisis. Pellentesque placerat elit a nunc. Nullam tortor odio, rutrum quis, egestas ut, posuere sed, felis. Vestibulum placerat feugiat nisl. Suspendisse lacinia, odio non feugiat vestibulum, sem erat blandit metus, ac nonummy magna odio pharetra felis. Vivamus vehicula velit non metus faucibus auctor. Nam sed augue. Donec orci. Cras eget diam et dolor dapibus sollicitudin. In lacinia, tellus vitae laoreet ultrices, lectus ligula dictum dui, eget condimentum velit</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="thumbnail clearfix">
-                            <img src="{{URL::asset('img/apache_pb.png')}}" alt="..." class="img-thumbnail pull-left">
-                            <h3>Meno priezvisko</h3>
-                            <p><span class="glyphicon glyphicon-envelope"></span> jozef.d13@gmail.com</p>
-                            <p><span class="glyphicon glyphicon-map-marker"></span> FMFI UK, Bratislava</p>    
-                            <p class="text-justify"><span class="glyphicon glyphicon-search"></span> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean et est a dui semper facilisis. Pellentesque placerat elit a nunc. Nullam tortor odio, rutrum quis, egestas ut, posuere sed, felis. Vestibulum placerat feugiat nisl. Suspendisse lacinia, odio non feugiat vestibulum, sem erat blandit metus, ac nonummy magna odio pharetra felis. Vivamus vehicula velit non metus faucibus auctor. Nam sed augue. Donec orci. Cras eget diam et dolor dapibus sollicitudin. In lacinia, tellus vitae laoreet ultrices, lectus ligula dictum dui, eget condimentum velit</p>
-                        </div>
-                    </div>
+            <div class="col-md-12">
+                <div class="thumbnail clearfix">
+                    <img src="{{URL::asset('img/apache_pb.png')}}" alt="..." class="img-thumbnail pull-left">
+                    <h3>Meno priezvisko</h3>
+                    <p><span class="glyphicon glyphicon-envelope"></span> jozef.d13@gmail.com</p>
+                    <p><span class="glyphicon glyphicon-map-marker"></span> FMFI UK, Bratislava</p>    
+                    <p class="text-justify"><span class="glyphicon glyphicon-search"></span> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean et est a dui semper facilisis. Pellentesque placerat elit a nunc. Nullam tortor odio, rutrum quis, egestas ut, posuere sed, felis. Vestibulum placerat feugiat nisl. Suspendisse lacinia, odio non feugiat vestibulum, sem erat blandit metus, ac nonummy magna odio pharetra felis. Vivamus vehicula velit non metus faucibus auctor. Nam sed augue. Donec orci. Cras eget diam et dolor dapibus sollicitudin. In lacinia, tellus vitae laoreet ultrices, lectus ligula dictum dui, eget condimentum velit</p>
                 </div>
             </div>
         </div>
-        <!-- sem loadovat uzivatelov-->
     </div>
-    <div class="col-md-4">
-        <div class="jumbotron">
-            <h2>Online geogebra časopis</h2>
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean et est a dui semper facilisis. Pellentesque placerat elit a nunc. Nullam tortor odio, rutrum quis, egestas ut, posuere sed, felis. Vestibulum placerat feugiat nisl. Suspendisse lacinia, odio non feugiat vestibulum, sem erat blandit metus, ac nonummy magna odio pharetra felis. Vivamus vehicula velit non metus faucibus auctor. Nam sed augue. Donec orci. Cras eget diam et dolor dapibus sollicitudin. In lacinia, tellus vitae laoreet ultrices, lectus ligula dictum dui, eget condimentum velit</p>
+    <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="thumbnail clearfix">
+                    <img src="{{URL::asset('img/apache_pb.png')}}" alt="..." class="img-thumbnail pull-left">
+                    <h3>Meno priezvisko</h3>
+                    <p><span class="glyphicon glyphicon-envelope"></span> jozef.d13@gmail.com</p>
+                    <p><span class="glyphicon glyphicon-map-marker"></span> FMFI UK, Bratislava</p>    
+                    <p class="text-justify"><span class="glyphicon glyphicon-search"></span> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean et est a dui semper facilisis. Pellentesque placerat elit a nunc. Nullam tortor odio, rutrum quis, egestas ut, posuere sed, felis. Vestibulum placerat feugiat nisl. Suspendisse lacinia, odio non feugiat vestibulum, sem erat blandit metus, ac nonummy magna odio pharetra felis. Vivamus vehicula velit non metus faucibus auctor. Nam sed augue. Donec orci. Cras eget diam et dolor dapibus sollicitudin. In lacinia, tellus vitae laoreet ultrices, lectus ligula dictum dui, eget condimentum velit</p>
+                </div>
+            </div>
         </div>
     </div>
+</div>
+<!-- sem loadovat uzivatelov-->
+@stop
+
+@section('right')
+<div class="jumbotron">
+    <h2>Online geogebra časopis</h2>
+    <p class="text-justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean et est a dui semper facilisis. Pellentesque placerat elit a nunc. Nullam tortor odio, rutrum quis, egestas ut, posuere sed, felis. Vestibulum placerat feugiat nisl. Suspendisse lacinia, odio non feugiat vestibulum, sem erat blandit metus, ac nonummy magna odio pharetra felis. Vivamus vehicula velit non metus faucibus auctor. Nam sed augue. Donec orci. Cras eget diam et dolor dapibus sollicitudin. In lacinia, tellus vitae laoreet ultrices, lectus ligula dictum dui, eget condimentum velit</p>
 </div>
 @stop
