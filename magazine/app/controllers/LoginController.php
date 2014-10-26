066a2d3019ef8421a17848d8c9f2fcd201881ca7
D0d0 <jozef.d13@gmail.com>
2014-10-26 23:59:55 +0100

    format

--- magazine/app/controllers/LoginController.php
+++ magazine/app/controllers/LoginController.php
@@ -7,48 +7,45 @@ class LoginController extends Controller {
             Auth::logout();
             return Redirect::to('/')
                             ->with('message', Lang::get('common.logout_successful'));
-        } else {
-            return Redirect::to('/')
-                            ->with('warning', Lang::get('common.acces_denied'));
         }
+        return Redirect::to('/')
+                        ->with('warning', Lang::get('common.acces_denied'));
     }
 
     public function getLogin() {
         if (Auth::check()) {
             return Redirect::to('/')
                             ->with('warning', Lang::get('common.acces_denied'));
-        } else {
-            return View::make('login');
         }
+        return View::make('login');
     }
 
     public function postLogin() {
         if (Auth::check()) {
             return Redirect::to('/')
                             ->with('warning', Lang::get('common.acces_denied'));
-        } else {
-            $input['email'] = Input::get('email');
-            $input['password'] = Input::get('password');
-            $rules = array(
-                'email' => 'required|email|exists:users,email',
-                'password' => 'required|min:6|'
-            );
-            $validator = Validator::make($input, $rules);
-            if ($validator->fails()) {
-                return Redirect::back()
-                                ->withErrors($validator)
-                                ->withInput(Input::except('password'));
-            }
-            if (Auth::attempt(array(
-                        'email' => Input::get('email'),
-                        'password' => Input::get('password'),
-                        'confirmed' => 1), Input::get('remember'))) {
-                return Redirect::to('/')
-                                ->with('message', Lang::get('common.login_successful'));
-            }
+        }
+        $input['email'] = Input::get('email');
+        $input['password'] = Input::get('password');
+        $rules = array(
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|min:6|'
+        );
+        $validator = Validator::make($input, $rules);
+        if ($validator->fails()) {
             return Redirect::back()
+                            ->withErrors($validator)
                             ->withInput(Input::except('password'));
         }
+        if (Auth::attempt(array(
+                    'email' => Input::get('email'),
+                    'password' => Input::get('password'),
+                    'confirmed' => 1), Input::get('remember'))) {
+            return Redirect::to('/')
+                            ->with('message', Lang::get('common.login_successful'));
+        }
+        return Redirect::back()
+                        ->withInput(Input::except('password'));
     }
 
 }
--- magazine/app/controllers/RemindersController.php
+++ magazine/app/controllers/RemindersController.php
@@ -11,9 +11,8 @@ class RemindersController extends Controller {
         if (Auth::check()) {
             return Redirect::to('/')
                             ->with('warning', Lang::get('common.acces_denied'));
-        } else {
-            return View::make('password.remind');
         }
+        return View::make('password.remind');
     }
 
     /**
@@ -25,14 +24,13 @@ class RemindersController extends Controller {
         if (Auth::check()) {
             return Redirect::to('/')
                             ->with('warning', Lang::get('common.acces_denied'));
-        } else {
-            switch ($response = Password::remind(Input::only('email'))) {
-                case Password::INVALID_USER:
-                    return Redirect::back()->with('error', Lang::get($response));
+        }
+        switch ($response = Password::remind(Input::only('email'))) {
+            case Password::INVALID_USER:
+                return Redirect::back()->with('error', Lang::get($response));
 
-                case Password::REMINDER_SENT:
-                    return Redirect::back()->with('status', Lang::get($response));
-            }
+            case Password::REMINDER_SENT:
+                return Redirect::back()->with('status', Lang::get($response));
         }
     }
 
@@ -46,12 +44,11 @@ class RemindersController extends Controller {
         if (Auth::check()) {
             return Redirect::to('/')
                             ->with('warning', Lang::get('common.acces_denied'));
-        } else {
-            if (is_null($token)) {
-                App::abort(404);
-            }
-            return View::make('password.reset')->with('token', $token);
         }
+        if (is_null($token)) {
+            App::abort(404);
+        }
+        return View::make('password.reset')->with('token', $token);
     }
 
     /**
@@ -63,27 +60,26 @@ class RemindersController extends Controller {
         if (Auth::check()) {
             return Redirect::to('/')
                             ->with('warning', Lang::get('common.acces_denied'));
-        } else {
-            $credentials = Input::only(
-                            'email', 'password', 'password_confirmation', 'token'
-            );
+        }
+        $credentials = Input::only(
+                        'email', 'password', 'password_confirmation', 'token'
+        );
 
-            $response = Password::reset($credentials, function($user, $password) {
-                        $user->password = Hash::make($password);
+        $response = Password::reset($credentials, function($user, $password) {
+                    $user->password = Hash::make($password);
 
-                        $user->save();
-                    });
+                    $user->save();
+                });
 
-            switch ($response) {
-                case Password::INVALID_PASSWORD:
-                case Password::INVALID_TOKEN:
-                case Password::INVALID_USER:
-                    return Redirect::back()
-                        ->with('error', Lang::get($response));
+        switch ($response) {
+            case Password::INVALID_PASSWORD:
+            case Password::INVALID_TOKEN:
+            case Password::INVALID_USER:
+                return Redirect::back()
+                                ->with('error', Lang::get($response));
 
-                case Password::PASSWORD_RESET:
-                    return Redirect::to('/');
-            }
+            case Password::PASSWORD_RESET:
+                return Redirect::to('/');
         }
     }
 
