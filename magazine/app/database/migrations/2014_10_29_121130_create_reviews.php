813f4988af34386d79c365940d3bc7a647d1d9d9
D0d0 <jozef.d13@gmail.com>
2014-10-29 13:48:50 +0100

    fix timestamps

--- magazine/app/database/migrations/2014_10_29_121130_create_reviews.php
+++ magazine/app/database/migrations/2014_10_29_121130_create_reviews.php
@@ -5,23 +5,20 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateReviews extends Migration {
 
-	public function up()
-	{
-         Schema::create('reviews', function(Blueprint $table) {
+    public function up() {
+        Schema::create('reviews', function(Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->integer('id_article')->unsigned();
             $table->foreign('id_article')->references('id')->on('article');
             $table->integer('reviewer_id')->unsigned();
             $table->foreign('reviewer_id')->references('id')->on('users');
             $table->string('text');
-            $table->timestamp('time');
-            });
-	}
+            $table->timestamps();
+        });
+    }
 
-
-	public function down()
-	{
-            Schema::dropIfExists('reviews');	
-	}
+    public function down() {
+        Schema::dropIfExists('reviews');
+    }
 
 }
