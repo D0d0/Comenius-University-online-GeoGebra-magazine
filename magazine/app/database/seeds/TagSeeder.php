d20830726b2e7caec5cc953c784231c2216e2b12
Michal Hoľa <hola2@uniba.sk>
2014-11-25 00:36:15 -0500

    seed more tags

--- magazine/app/database/seeds/TagSeeder.php
+++ magazine/app/database/seeds/TagSeeder.php
@@ -3,10 +3,12 @@
 class TagSeeder extends Seeder {
     public function run() {
         $faker = Faker\Factory::create();
-        for ($i=0; $i<100; $i++) {
+        // create many tags...:
+        for ($i=0; $i<500; $i++) {
             Tag::create(array(
                'id_tag' => $faker->numberBetween(1, 10),
-               'id_article' => $faker->numberBetween(1, 100),
+               // not every article will have tag...:
+               'id_article' => $faker->numberBetween(1, 90),
             ));
         }
     }
